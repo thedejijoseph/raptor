ca41770d04a22f87ceeb244e697da7729bea2217
Deji <wrecodde@gmail.com>
2022-06-27 20:33:07 +0100

    fix: fetch data from more than one page

--- src/util.py
+++ src/util.py
@@ -4,6 +4,8 @@ from environs import Env
 
 from datetime import datetime
 
+from numpy import number
+
 env = Env()
 env.read_env()
 
@@ -28,6 +30,18 @@ def call_issues_endpoint(owner, repo, state="all"):
         # log an error here
         return []
     
+    issue_count = data[0]['number'] # the number of all prs created is the number of the last one
+    number_of_pages = (issue_count // 100) + 1
+
+    if number_of_pages > 1:
+        accumulator = data
+        for page in range(2, number_of_pages + 1):
+            query_params['page'] = page
+            resp = httpx.get(f"https://api.github.com/repos/{owner}/{repo}/pulls", headers=headers, params=query_params)
+            data = resp.json()
+            accumulator.extend(data)
+        data = accumulator
+    
     for item in data:
         item['user'] = item['user']['login']
         item['repo'] = f"{owner}/{repo}"
@@ -47,7 +61,9 @@ def call_pulls_endpoint(owner, repo, state="all"):
         "Authorization": f"token {GH_TOKEN}"
     }
     query_params = {
-        "state": state
+        "state": state,
+        "per_page": 100,
+        "page": 1
     }
     
     resp = httpx.get(f"https://api.github.com/repos/{owner}/{repo}/pulls", headers=headers, params=query_params)
@@ -56,6 +72,18 @@ def call_pulls_endpoint(owner, repo, state="all"):
     if resp.status_code not in (200, 304):
         # log an error here
         return []
+    
+    pull_count = data[0]['number'] # the number of all prs created is the number of the last one
+    number_of_pages = (pull_count // 100) + 1
+
+    if number_of_pages > 1:
+        accumulator = data
+        for page in range(2, number_of_pages + 1):
+            query_params['page'] = page
+            resp = httpx.get(f"https://api.github.com/repos/{owner}/{repo}/pulls", headers=headers, params=query_params)
+            data = resp.json()
+            accumulator.extend(data)
+        data = accumulator
 
     for item in data:
         item['user'] = item['user']['login']
